c02258dab1a1c0c9f580423a2a7e45e53ae485af
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 06:37:15 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "5b238281-bfc3-472b-9210-3d0fbcff1d0b",
+    "home_feed": [
+        {
+            "id": "bda6ca40-8a43-4c43-9605-9a1d5e63b84b",
+            "artist_name": "field",
+            "home_carousel": [
+                {
+                    "id": "d151ec27-fa79-4834-b014-c083d68513d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/74.png"
+                },
+                {
+                    "id": "a0640f9a-d41e-4b0e-ac6f-09d65fc62d01",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/235.png"
+                },
+                {
+                    "id": "c2e2b2fb-80e9-40cd-b876-09691b48543e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png"
+                },
+                {
+                    "id": "b27f6835-57d9-4c45-81df-99638071dc37",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/139.png"
+                },
+                {
+                    "id": "b5af5296-8e30-4cfd-9cbf-b1b1df1b23e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/323.png"
+                }
+            ],
+            "art_caption": "response",
+            "art_description": "Stay her son school member sort research. Open type leader green. Pay six have sound wind."
+        },
+        {
+            "id": "e7fba70e-2886-4923-a3aa-4d33583ed765",
+            "artist_name": "type",
+            "home_carousel": [
+                {
+                    "id": "42cf962b-b862-4de3-a6ae-ca556f2a4088",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png"
+                },
+                {
+                    "id": "5b880614-2aeb-4982-8337-494899e5da70",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+                },
+                {
+                    "id": "a00d72e3-e4a8-467a-89c7-7e4f725322fc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png"
+                },
+                {
+                    "id": "64a890b1-8e46-4501-b777-4234e720668c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/195.png"
+                },
+                {
+                    "id": "a6be1c68-80a3-4771-8176-ac2cb6c08f0d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/162.png"
+                }
+            ],
+            "art_caption": "war",
+            "art_description": "Interesting build dinner minute after toward remember. Return use help."
+        },
+        {
+            "id": "4ddd41f3-1128-4c8f-9bf5-677212e4dccc",
+            "artist_name": "may",
+            "home_carousel": [
+                {
+                    "id": "73413827-5e47-4da7-b420-f622aef51710",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/147.png"
+                },
+                {
+                    "id": "1ea5501e-5a89-4518-9948-2be92bd4929d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png"
+                },
+                {
+                    "id": "8a9378a1-13b8-4807-add3-cf56f6f1f526",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/154.png"
+                },
+                {
+                    "id": "c1496837-2a13-4856-9e1c-fb030189fbbb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/313.png"
+                },
+                {
+                    "id": "c49de9a8-fa35-4077-ad66-119b55c7fbbf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/264.png"
+                }
+            ],
+            "art_caption": "agent",
+            "art_description": "How imagine measure too probably garden strategy. Choose recognize for near argue."
+        },
+        {
+            "id": "0822b09f-844e-4d00-ad41-f0ad0c45c8a6",
+            "artist_name": "carry",
+            "home_carousel": [
+                {
+                    "id": "da95152a-fdea-4de9-94c3-fd22af956db9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/353.png"
+                },
+                {
+                    "id": "d0402364-f587-4c46-b6d3-f08ebf89031b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/263.png"
+                },
+                {
+                    "id": "3505d900-a09f-4509-8541-49eeabf1ad47",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "f76f42a2-1af2-41a0-822c-1d50e28606b1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png"
+                },
+                {
+                    "id": "59c0ddd9-49da-40ef-a3aa-605448f7d851",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png"
+                }
+            ],
+            "art_caption": "tough",
+            "art_description": "Only tell agent from air special a. Choose pass than east base long mouth. Realize person someone effect then."
+        },
+        {
+            "id": "d32ca33f-2091-4e98-b9ec-e87b1b1f31e9",
+            "artist_name": "kitchen",
+            "home_carousel": [
+                {
+                    "id": "64cbcf04-678c-4edf-a12c-eceffa523e25",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/24.png"
+                },
+                {
+                    "id": "c74896b7-b0f5-4f11-88ec-5706d959d16f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+                },
+                {
+                    "id": "6bbbe81d-887f-464c-b721-3f37eafc3c65",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "a5b3866a-f0e4-44b9-af3a-be935ab2c24f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/241.png"
+                },
+                {
+                    "id": "110a82fb-1cb9-4f2b-8938-84270f31eea3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png"
+                }
+            ],
+            "art_caption": "air",
+            "art_description": "Ahead interest manage may center surface. Door suffer local everyone chance situation."
+        }
+    ]
+}
